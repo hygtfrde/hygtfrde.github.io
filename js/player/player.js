a0165be88c045d8114437725ff0cf70258fefa4f
harold <haroldulrich@protonmail.com>
2023-01-18 16:00:31 -0800

    refactor movement function

---
+++ js/player/player.js
@@ -0,0 +1,12 @@
+let player = {
+    x: width - 100,
+    y: height - 15,
+    width: 50,
+    height: 50,
+    speed: 5,
+    velocity_X: 0,
+    velocity_Y: 0,
+  
+    jumping: false,
+    grounded: false
+  };
