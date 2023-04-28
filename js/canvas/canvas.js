dde47c79976753fd13d51d295a1e628b8e87ad88
harold <haroldulrich@protonmail.com>
2023-04-28 15:00:44 -0700

    canvas size adjustment alert

--- js/canvas/canvas.js
+++ js/canvas/canvas.js
@@ -2,12 +2,16 @@
 let canvas = document.getElementById("canvas")
 context = canvas.getContext("2d");
 
-canvas.width = window.innerWidth - 271;
-canvas.height = window.innerHeight - 314;
+canvas.width = window.innerWidth - (271);
+canvas.height = window.innerHeight - (314);
 
 document.body.onresize = () => {
-    canvas.width = window.innerWidth - 271;
-    canvas.height = window.innerHeight - 314;
+        canvas.width = window.innerWidth - (271);
+        canvas.height = window.innerHeight - (314);
+}
+
+if (window.innerWidth <= 500 || window.innerHeight <= 500) {
+    alert('window too small, please use a larger size')
 }
 
 (function () {
